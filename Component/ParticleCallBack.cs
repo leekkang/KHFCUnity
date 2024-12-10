752bad88fafece646179367f064a4441f2569452
kanghyuk <qkaxhfdl93@gmail.com>
2024-12-10 18:26:31 +0900

    add new scripts

---
+++ Component/ParticleCallBack.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KHFC {
+	public class ParticleOverCallBack : MonoBehaviour {
+		System.Action m_OnComplete;
+
+		void Start() {
+			var main = GetComponent<ParticleSystem>().main;
+			main.stopAction = ParticleSystemStopAction.Callback;
+		}
+
+		private void OnParticleSystemStopped() {
+			m_OnComplete?.Invoke();
+		}
+	}
+}
