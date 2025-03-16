11b43d32cb3a835a2c03d1f975f7c7093bd81542
LEEKKANG-HOME\Kanghyuk <qkaxhfdl93@gmail.com>
2025-03-16 17:57:07 +0900

    organize code

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,6 +2,7 @@
 
 using System.Runtime.CompilerServices;
 
-// 에디터 어셈블리에서 인터널 메소드 사용 가능하도록 변경
+// 현재 어셈블리에서 에디터 어셈블리에 인터널 메소드를 보여줌
+// -> 에디터 어셈블리에서 현재 어셈블리의 인터널 메소드 사용 가능하도록 변경
 // assembly 키워드는 attribute를 어셈블리에 적용하도록 변경하는 역할을 한다.
 [assembly: InternalsVisibleTo("Assembly-CSharp-Editor")]
--- GameState/AbstractGameState.cs
+++ GameState/AbstractGameState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 namespace KHFC {
 	public abstract class AbstractGameState : MonoBehaviour {
 		public int m_StateIndex;
+
 		public virtual void OnEnter(int prevStateIndex) { }
 		public virtual void OnLeave(int nextStateIndex) { }
 	}
